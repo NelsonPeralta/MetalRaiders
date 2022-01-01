de127eee50c11b8a40f7d45da45153a229552fd6
Nelson <nelsonperalta.prog@gmail.com>
2021-12-31 19:58:26 -0500

    Fireball fix

--- Assets/002 - Scripts/New Scripts/AI Scripts/Fireball.cs
+++ Assets/002 - Scripts/New Scripts/AI Scripts/Fireball.cs
@@ -19,6 +19,7 @@ public class Fireball : MonoBehaviour
     [Header("Prefabs")]
     public Transform explosionPrefab;
 
+    List<PlayerProperties> playersHit = new List<PlayerProperties>();
     GameObject[] AIsHit = new GameObject[20];
 
     private void Start()
@@ -67,29 +68,25 @@ public class Fireball : MonoBehaviour
         foreach (Collider hit in colliders)
         {
             Rigidbody rb = hit.GetComponent<Rigidbody>();
+            float hitDistance = Vector3.Distance(hit.transform.position, transform.position);
+            float calculatedDamage = damage * (1 - (hitDistance / radius));
 
             //Add force to nearby rigidbodies
             if (rb != null)
                 rb.AddExplosionForce(power * 5, explosionPos, radius, 3.0F);
 
-            if (hit.GetComponent<PlayerHitbox>() != null)
+            if (hit.GetComponent<PlayerHitbox>() && !playersHit.Contains(hit.GetComponent<PlayerProperties>()))
             {
-                //Debug.Log("Fireball Hit Player");
                 GameObject player = hit.GetComponent<PlayerHitbox>().player.gameObject;
                 float playerDistance = Vector3.Distance(hit.transform.position, transform.position);
-
                 int playerHitID = player.GetComponent<PlayerProperties>().playerRewiredID;
 
 
-                if (!player.GetComponent<PlayerProperties>().isDead)
+                if (playerDistance < radius)
                 {
-                    if (playerDistance < radius)
-                    {
-                        float calculatedDamage = damage * (1 - (playerDistance / radius));
-                        //Debug.Log("Damage= " + damage + " playerDistance= " + playerDistance + " radius= " + radius);
-                        player.GetComponent<PlayerProperties>().Damage((int)calculatedDamage, false, 99);
-
-                    }
+                    //Debug.Log("Damage= " + damage + " playerDistance= " + playerDistance + " radius= " + radius);
+                    player.GetComponent<PlayerProperties>().Damage((int)calculatedDamage, false, 99);
+                    playersHit.Add(player.GetComponent<PlayerProperties>());
                 }
             }
             if (hit.GetComponent<AIHitbox>() != null)
@@ -130,7 +127,6 @@ public class Fireball : MonoBehaviour
                 {
                     if (hit.GetComponent<AIHitbox>().aiHealth > 0)
                     {
-                        float calculatedDamage = damage * (1 - (aiDistance / radius));
                         hit.GetComponent<AIHitbox>().DamageAI(false, calculatedDamage, playerWhoThrewGrenade);
                     }
 
