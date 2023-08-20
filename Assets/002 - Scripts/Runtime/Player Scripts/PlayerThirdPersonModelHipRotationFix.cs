3691649bf7673457932b5bb5d69b71f345202708
NelsonPeralta <76981043+NelsonPeralta@users.noreply.github.com>
2023-08-19 23:52:36 -0400

    Hip rotation fix tweak

--- Assets/002 - Scripts/Runtime/Player Scripts/PlayerThirdPersonModelHipRotationFix.cs
+++ Assets/002 - Scripts/Runtime/Player Scripts/PlayerThirdPersonModelHipRotationFix.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class PlayerThirdPersonModelHipRotationFix : MonoBehaviour
 {
+    float currentRotationFix
+    {
+        get { return _currentRotationFix; }
+        set
+        {
+            //_currentRotationFix = Mathf.RoundToInt(value / 10) * 10;
+
+            _currentRotationFix = Mathf.Clamp(value, 0, 30);
+        }
+    }
+
     [SerializeField] Player _player;
+    [SerializeField] float _currentRotationFix = 0;
 
     int _rotationTarget = 30;
 
     private void Update()
     {
-        if (_player.movement.speedRatio == 0)
-            transform.localRotation = Quaternion.Euler(0, _rotationTarget, 0);
+        if (_player.movement.speedRatio > 0)
+            currentRotationFix -= Time.deltaTime * _rotationTarget * 5;
         else
-            transform.localRotation = Quaternion.Euler(0, _rotationTarget - _player.movement.speedRatio * _rotationTarget, 0);
+            currentRotationFix += Time.deltaTime * _rotationTarget * 5;
+        transform.localRotation = Quaternion.Euler(0, currentRotationFix, 0);
     }
 }
