38ed77ee940f7c8f0d2171f4ddd6dac01760e293
NelsonPeralta <76981043+NelsonPeralta@users.noreply.github.com>
2024-04-09 22:32:03 -0400

    UI weapon icon overhaul

--- Assets/002 - Scripts/Runtime/GUI/OnEnableIncreaseScaleThenScaleBack.cs
+++ Assets/002 - Scripts/Runtime/GUI/OnEnableIncreaseScaleThenScaleBack.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using PlasticGui.WorkspaceWindow.PendingChanges;
 using UnityEngine;
 
 public class OnEnableIncreaseScaleThenScaleBack : MonoBehaviour
